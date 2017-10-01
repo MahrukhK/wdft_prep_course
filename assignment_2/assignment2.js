b432bfd2de42e7e3baadd52b43c3fd7197383ba5
Mahrukh <mahrukh@Mahrukhs-MacBook-Pro.local>
2017-09-30 21:30:23 -0400

    first code push

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -2,12 +2,19 @@
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
+var (i = 0; i < 101; i++) {
+    console.log(i)
+}
+console.log ('loop is done')
 
 
 
 /*
 Exercise #2
 Write a loop that makes seven calls to console.log to output the following triangle:
+
+
+
 
 #
 ##
